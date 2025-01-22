9943a4ebed613c4d90363cf1729df671cb0ae4e9
monobeartae <guanquntan@gmail.com>
2025-01-23 06:12:29 +0800

    Mark As Done

--- src/main/java/MonoBot.java
+++ src/main/java/MonoBot.java
@@ -32,24 +32,61 @@ public class MonoBot {
     }
 
     public void processInput(String input) {
-        switch (input) {
-            case "bye":
-                this.StopBot();
+        if (input.equals("bye")) {
+            this.StopBot();
+            return;
+        }
+        if (input.equals("list")) {
+            this.PrintTaskList();
+            return;
+        }
+        String[] split = input.split(" ", 2);
+        String cmd = split[0];
+        switch (cmd) {
+            case "add":
+                String name = split[1];
+                this.AddTask(new Task(name));
+                this.printMessage("added: " + name);
                 break;
-            case "list":
-                this.PrintTaskList();
+            case "mark":
+                int idx = Integer.parseInt(split[1]);
+                this.MarkTaskComplete(idx);
+                break;
+            case "unmark":
+                int task_idx = Integer.parseInt(split[1]);
+                this.UnmarkCompletedTask(task_idx);
                 break;
             default:
-                this.AddTask(new Task(input));
-                this.printMessage("added: " + input);
+                this.printMessage("Unknown Command :o");
                 break;
         }
+    }
+
+    private void MarkTaskComplete(int idx) {
+        if (idx > this.tasklist.size()) {
+            this.printErrorMessage("Task " + idx + " does not exist! Enter 'list' to view list of tasks.");
+            return;
+        }
+        if (!this.tasklist.get(idx - 1).MarkAsComplete())
+            this.printErrorMessage("Task " + idx + " is already completed!");
+        else
+            this.printMessage("Task " + idx + " has been marked complete!");
+    }
+    private void UnmarkCompletedTask(int idx) {
+        if (idx > this.tasklist.size()) {
+            this.printErrorMessage("Task " + idx + " does not exist! Enter 'list' to view list of tasks.");
+            return;
+        }
+        if (!this.tasklist.get(idx - 1).UnmarkCompleted())
+             this.printErrorMessage("Task " + idx + " has not been completed!");
+        else
+            this.printMessage("Task " + idx + " has been unmarked!");
     }
 
     private void PrintTaskList() {
         String[] list = new String[this.tasklist.size()];
         for (int i = 0; i < this.tasklist.size(); i++)
-            list[i] = String.format("%d. %s", i, tasklist.get(i).toString());
+            list[i] = String.format("%d. %s", i + 1, tasklist.get(i).toString());
         this.printMessage(list);
     }
 
@@ -62,6 +99,10 @@ public class MonoBot {
         System.out.println(this.INDENT + this.SEPARATOR);
         System.out.println(this.INDENT + msg);
         System.out.println(this.INDENT + this.SEPARATOR);
+    }
+
+    private void printErrorMessage(String context) {
+        this.printMessage("\\(T o T)'/ " + context);
     }
 
     private void printMessage(String[] msg) {
--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,13 +1,27 @@
 package tasks;
 
 public class Task {
-    protected String taskName;
+    private String taskName;
+    private boolean isCompleted;
 
     public Task(String name) {
         this.taskName = name;
+        this.isCompleted = false;
+    }
+    public boolean MarkAsComplete() {
+        if (this.isCompleted)
+            return false;
+        this.isCompleted = true;
+        return true;
+    }
+    public boolean UnmarkCompleted() {
+        if (!this.isCompleted)
+            return false;
+        this.isCompleted = false;
+        return true;
     }
     @Override
     public String toString() {
-        return taskName;
+        return (this.isCompleted ? "[X] " : "[ ] ") + this.taskName;
     }
 }
