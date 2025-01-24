dcb91d797cad0baf6bbf40aa1777d75529ed0a37
monobeartae <guanquntan@gmail.com>
2025-01-24 10:36:25 +0800

    Automated Test UI

--- src/main/java/UserInputHandler.java
+++ src/main/java/UserInputHandler.java
@@ -1,14 +1,28 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UserInputHandler {
     private Scanner sc;
+    private InputType inputType;
+
+    enum InputType {
+        Manual,
+        Auto
+    }
 
     public UserInputHandler() {
         sc = new Scanner(System.in);
+        try {
+            inputType = System.in.available() > 0 ? InputType.Auto : InputType.Manual;
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
     }
     public String getUserInput() {
         System.out.print("Input: ");
         String input = sc.nextLine();
+        if (inputType == InputType.Auto)
+            System.out.println();
         return input;
     }
 }
